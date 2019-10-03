c451ba34971074f0b2d59d2f9cc1949ed1f7dee9
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-10-03 16:54:04 +0700

    Update project

---
+++ DataAccess/SQLServer.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataAccess
+{
+	/// <summary>
+	/// Lớp SQL Server
+	/// cung cấp các phương thức trên hệ quản trị cơ sở dữ liệu SQL Server 
+	/// </summary>
+	public class SQLServer
+	{
+		public List<string> MyServers { get; set; }
+
+		public SQLServer()
+		{
+			this.MyServers = new List<string>();
+		}
+
+		/// <summary>
+		/// Tìm các SQL Server trên máy (không đầy đủ)
+		/// </summary>
+		public void FindServers()
+		{
+			string ServerName = Environment.MachineName;
+			RegistryView registryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+			using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+			{
+				RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
+				if (instanceKey != null)
+				{
+					foreach (var instanceName in instanceKey.GetValueNames())
+					{
+						this.MyServers.Add(ServerName + "\\" + instanceName);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Đọc tên SQL Server từ file
+		/// </summary>
+		/// <param name="path">Đường dẫn đến file</param>
+		public void ReadFromFile(string path)
+		{
+			using (var reader = new StreamReader(path))
+			{
+				while (!reader.EndOfStream)
+				{
+					string name = reader.ReadLine();
+					if (!this.MyServers.Contains(name.Trim()))
+					{
+						this.MyServers.Add(name.Trim());
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Ghi tên SQL Server ra file
+		/// </summary>
+		/// <param name="path">Đường dẫn đến file</param>
+		public void WriteToFile(string path)
+		{
+			StringBuilder data = new StringBuilder();
+			foreach (var item in this.MyServers)
+			{
+				data.AppendLine(item);
+			}
+			File.WriteAllText(path, data.ToString()); //Ghi đè nếu không tồn tại!
+		}
+
+		/// <summary>
+		/// Kiểm tra kết nối đến một server
+		/// Sử dụng Windows Authentication
+		/// </summary>
+		/// <param name="server">Tên server</param>
+		/// <returns>Kết nối thành công</returns>
+		public bool TestConnection(string server)
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Kiểm tra kết nối đến một server
+		/// Sử dụng User Authentication
+		/// </summary>
+		/// <param name="server">Tên server</param>
+		/// <param name="username">Tên đăng nhập, mặc định là: sa</param>
+		/// <param name="password">Mật khẩu</param>
+		/// <returns>Kết nối thành công</returns>
+		public bool TestConnection(string server, string username, string password)
+		{
+			return true;
+		}
+
+	}
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Testing
+{
+	class Program
+	{
+		static void Main()
+		{
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
