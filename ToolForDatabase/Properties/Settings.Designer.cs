1de820900e4261eef5291201ed5a12f67feb4816
La Quốc Thắng <quocthang0507@gmail.com>
2019-12-09 17:35:53 +0700

    Improve UI

--- ToolForDatabase/Properties/Settings.Designer.cs
+++ ToolForDatabase/Properties/Settings.Designer.cs
@@ -46,17 +46,5 @@ namespace ToolForDatabase.Properties {
                 this["Database"] = value;
             }
         }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Language {
-            get {
-                return ((string)(this["Language"]));
-            }
-            set {
-                this["Language"] = value;
-            }
-        }
     }
 }
